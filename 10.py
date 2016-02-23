ebbad429452078fdc3fbf5e9e9824f6fe2193cf6
mrbaozi <mrbaozi@me.com>
2016-02-23 13:58:18 +0100

    Clean 10.py

--- 10.py
+++ 10.py
@@ -1,19 +1,19 @@
 def is_prime(n):
-    if (n < 1):
+    if n < 1:
         return False
-    if (n <= 3):
+    if n <= 3:
         return True
     if (n%2 == 0) or (n%3 == 0):
         return False
     i = 5
-    while (i*i <= n):
+    while i*i <= n:
         if (n%i == 0) or (n%(i+2) == 0):
             return False
         i += 6
     return True
 
-sum = 2
+S = 2
 for i in range(3, 2000000, 2):
     if is_prime(i):
-        sum += i
-print(sum)
+        S += i
+print(S)
